e446f4ac9d2ebd6860762a84c2698daf0578d16a
yuhaojun <13840526193@163.com>
2025-07-10 23:34:00 +0800

    初始化项目

---
+++ src/main/java/com/easymeeting/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.easymeeting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("health")
+    public String health() {
+        return "ok";
+    }
+}
